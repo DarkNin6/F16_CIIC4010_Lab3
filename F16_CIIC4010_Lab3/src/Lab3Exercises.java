74f66da5a2e563cfcf207a706a0ed71f9bbb9d04
Chris <Chris@192.168.0.102>
2016-09-01 00:09:14 -0400

    !) Added Polygons and changed the shape of the oval and made it so it does not move from the center.
    2) Connected MyPanelClasses to Lab3Exercises to show the shapes
    visually.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,9 +3,15 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Christopher Roman");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
-		myFrame.setSize(200, 200);
+		myFrame.setSize(600, 600);
 		myFrame.setVisible(true);
+		
+		 MyPanelClass myPanel = new MyPanelClass();
+         myFrame.getContentPane().add(myPanel);
+         
+
+         
 	}
 }
